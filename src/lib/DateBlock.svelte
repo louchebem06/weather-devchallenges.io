74fd5853eaf5d8422c61cf08c348f3c45e62a28a
bledda <bledda@student.42nice.fr>
2023-02-18 19:05:04 +0100

    update
    - add dateblock

---
+++ src/lib/DateBlock.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  	import type { Weather } from "$lib/weatherApi";
+
+	export let weather: Weather;
+	const date: Date = new Date(weather.dt * 1000);
+	let dateString: string
+		= date.toLocaleDateString("en-US", {
+				weekday: "short",
+				month: "short",
+				day: "numeric"
+			});
+	
+	const splitDate = dateString.split(' ');
+	dateString = `${splitDate[0]} ${splitDate[2]} ${splitDate[1]}`; 
+</script>
+
+<div class="date">
+	<p>Today</p>
+	<p>•</p>
+	<p>{dateString}</p>
+</div>
+
+<style>
+	.date {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.date {
+		padding-bottom: 32px;
+		gap: 16px;
+		color: #88869D;
+	}
+</style>
--- src/lib/Panel.svelte
+++ src/lib/Panel.svelte
@@ -3,6 +3,7 @@
 	import Hero from "$lib/Hero.svelte";
 	import Temp from "$lib/Temp.svelte";
   	import SearchBlock from "$lib/SearchBlock.svelte";
+	import DateBlock from "$lib/DateBlock.svelte";
 
 	export let celsius: boolean;
 	export let weather: Weather;
@@ -10,16 +11,6 @@
 	export let week: Week;
 
 	let search: boolean = false;
-	const date: Date = new Date(weather.dt * 1000);
-	let dateString: string
-		= date.toLocaleDateString("en-US", {
-				weekday: "short",
-				month: "short",
-				day: "numeric"
-			});
-	
-	const splitDate = dateString.split(' ');
-	dateString = `${splitDate[0]} ${splitDate[2]} ${splitDate[1]}`; 
 </script>
 
 <!-- TODO create div in composante -->
@@ -30,12 +21,7 @@
 		<Hero bind:weather={weather} />
 		<Temp bind:weather={weather} bind:celsius={celsius}/>
 		<h3>{weather.weather[0].description}</h3>
-		
-		<div class="date">
-			<p>Today</p>
-			<p>•</p>
-			<p>{dateString}</p>
-		</div>
+		<DateBlock bind:weather={weather}/>
 
 		<div class="location">
 			<span class="material-symbols-outlined">
@@ -70,7 +56,7 @@
 		}
 	}
 
-	.date, .location {
+	.location {
 		display: flex;
 		justify-content: center;
 		align-items: center;
@@ -83,17 +69,8 @@
 		text-transform: capitalize;
 	}
 
-	.date {
-		padding-bottom: 32px;
-		gap: 16px;
-	}
-
 	.location {
 		gap: 9px;
-	}
-
-	.date, .location {
 		color: #88869D;
 	}
-
 </style>
